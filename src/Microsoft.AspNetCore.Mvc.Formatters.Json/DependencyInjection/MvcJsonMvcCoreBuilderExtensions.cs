fade9e179fff815cd9ea8afdac9173e81e5c40f6
Neil Owen <neowen@microsoft.com>
2018-05-30 16:02:55 -0700

    Add extension methods to MvcServiceCollection to simplify configuration of Camel or Pascal casing.

--- src/Microsoft.AspNetCore.Mvc.Formatters.Json/DependencyInjection/MvcJsonMvcCoreBuilderExtensions.cs
+++ src/Microsoft.AspNetCore.Mvc.Formatters.Json/DependencyInjection/MvcJsonMvcCoreBuilderExtensions.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc.Formatters.Json.Internal;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -80,6 +81,76 @@ namespace Microsoft.Extensions.DependencyInjection
             services.TryAddEnumerable(
                 ServiceDescriptor.Transient<IApiDescriptionProvider, JsonPatchOperationsArrayProvider>());
             services.TryAddSingleton<JsonResultExecutor>();
+        }
+
+        /// <summary>
+        /// Configure json responses to use camel casing. 
+        /// </summary>
+        /// <param name="builder">The <see cref="IMvcCoreBuilder"/>.</param>
+        /// <param name="processDictionaryKeys">A flag indicating whether dictionary keys should be converted to camel casing.</param>
+        /// <param name="overrideSpecifiedNames">A flag indiciating whether explicitly provided json property names, using <see cref="JsonProperty"/> should be converted to camel casing.</param>
+        /// <param name="processExtensionDataNames">A flag indiciating whether json extention data properties should be converted to camel casing.</param>
+        /// <returns>The <see cref="IMvcCoreBuilder"/>.</returns>
+        public static IMvcCoreBuilder WithJsonCamelCasing(
+            this IMvcCoreBuilder builder,
+            bool processDictionaryKeys = false,
+            bool overrideSpecifiedNames = false,
+            bool processExtensionDataNames = false)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            builder.Services.Configure((MvcJsonOptions options) =>
+            {
+                options.SerializerSettings.ContractResolver = new DefaultContractResolver()
+                {
+                    NamingStrategy = new CamelCaseNamingStrategy()
+                    {
+                        OverrideSpecifiedNames = overrideSpecifiedNames,
+                        ProcessDictionaryKeys = processDictionaryKeys,
+                        ProcessExtensionDataNames = processExtensionDataNames
+                    }
+                };
+            });
+
+            return builder;
+        }
+
+        /// <summary>
+        /// Configure json responses to use pascal casing for all properties, including dictionary keys and properties with JsonProperty attribute
+        /// </summary>
+        /// <param name="builder">The <see cref="IMvcCoreBuilder"/>.</param>
+        /// <param name="processDictionaryKeys">A flag indicating whether dictionary keys should be converted to pascal casing.</param>
+        /// <param name="overrideSpecifiedNames">A flag indiciating whether explicitly provided json property names, using <see cref="JsonProperty"/> should be converted to pascal casing.</param>
+        /// <param name="processExtensionDataNames">A flag indiciating whether json extention data properties should be converted to pascal casing.</param>
+        /// <returns>The <see cref="IMvcCoreBuilder"/>.</returns>
+        public static IMvcCoreBuilder WithJsonPascalCasing(
+            this IMvcCoreBuilder builder,
+            bool processDictionaryKeys = false,
+            bool overrideSpecifiedNames = false,
+            bool processExtensionDataNames = false)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            builder.Services.Configure((MvcJsonOptions options) =>
+            {
+                options.SerializerSettings.ContractResolver = new DefaultContractResolver()
+                {
+                    NamingStrategy = new DefaultNamingStrategy()
+                    {
+                        OverrideSpecifiedNames = overrideSpecifiedNames,
+                        ProcessDictionaryKeys = processDictionaryKeys,
+                        ProcessExtensionDataNames = processExtensionDataNames
+                    }
+                };
+            });
+
+            return builder;
         }
     }
 }
